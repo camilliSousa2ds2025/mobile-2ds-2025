f423e8135c1b18c3c08b6fadfe253f50ae5e1d3c
CamilliSousa2ds2025 <sousa.camilli@escola.pr.gov.br>
2025-04-03 10:30:19 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import Aula01 from "./src/aula01";
+
 
 export default function App() {
   return (
-    
+    <Aula01 />
   );
 }
+
 
---
+++ src/aula01.jsx
@@ -0,0 +1,15 @@
+import React, { COMPONENT }from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class Aula01 extends Component{
+    render() {
+        return(
+            <View>
+                <Text>Estrutura Padrão! </Text>
+            </View>
+
+        );
+    }
+}
+
+export default Aula01;
