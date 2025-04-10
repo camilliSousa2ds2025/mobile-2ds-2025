8de4c7d1b9e90201922d4158e7eda679eab17d3d
CamilliSousa2ds2025 <sousa.camilli@escola.pr.gov.br>
2025-04-10 10:32:47 -0300

    Add files via upload

---
+++ src/aula02.jsx
@@ -0,0 +1,16 @@
+import React, { Component }from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class Aula02 extends Component{
+    render() {
+        return(
+            <View style={{flex: 1, backgroundColor:'black'}}>
+                <Text style={{color: 'white'}}>AULA 02 ESTILOS</Text>
+            </View>
+
+        );
+    }
+}
+
+export default Aula02;  
+
