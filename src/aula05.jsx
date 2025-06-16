39176e2d399dcf7072a05e9d7f5f84d7ebb01d81
CamilliSousa2ds2025 <sousa.camilli@escola.pr.gov.br>
2025-06-16 07:45:47 -0300

    Add files via upload

---
+++ src/aula05.jsx
@@ -0,0 +1,29 @@
+import React, { Component }from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class Aula05 extends Component{
+    render() {
+        return(
+            <View style={ styles.container }>
+                <Text style={ styles.texto}>Estrutura Padrão!</Text>
+            </View>
+
+        );
+    }
+}
+
+export default Aula05;  
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    texto: {
+        color: 'white',
+        fontSize: 50,
+    }
+})
