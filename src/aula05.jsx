ad91daf63f3cf90a8489f2a8b0a1ee6fbfe1f43b
CamilliSousa2ds2025 <sousa.camilli@escola.pr.gov.br>
2025-07-02 10:34:13 -0300

    Add files via upload

--- src/aula05.jsx
+++ src/aula05.jsx
@@ -1,29 +1,41 @@
-import React, { Component }from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React,{Component} from "react";
+import { View, Text, StyleSheet  } from "react-native";
 
 class Aula05 extends Component{
-    render() {
+    render(){
         return(
             <View style={ styles.container }>
-                <Text style={ styles.texto}>Estrutura Padrão!</Text>
+                <View style={ styles.caixa1 }>
+                    <Text style={{ color:'white'}}>Joice acompanhe a aula!</Text>
+                </View>
+                <View style={ styles.caixa2 }>2</View>
+                <View style={ styles.caixa3 }>3</View>
             </View>
-
+                
         );
     }
 }
-
-export default Aula05;  
+export default Aula05;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'black',
-        alignItems: 'center',
-        justifyContent: 'center'
+        backgroundColor: 'white',
+       
+    },
+    caixa1: {
+        backgroundColor: 'red',
+        height: 50
+    },
+    caixa2: {
+        backgroundColor: 'green',
+        flex: 1
+    },
+    caixa3: {
+        backgroundColor: 'blue',
+        height: 50
     },
 
-    texto: {
-        color: 'white',
-        fontSize: 50,
-    }
+
+
 })
